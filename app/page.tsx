54a366157effda03c4354f97ac392d284d2197d5
kiadimalala <kiady.rj@gmail.com>
2023-06-26 20:45:37 +0300

    Adding mission section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import styles from "./page.module.css";
 
-import { Banner, HowItWork } from "@app/components";
+import { Banner, HowItWork, Mission } from "@app/components";
 
 export default function Home() {
   return (
     <div className={styles.app}>
       <Banner />
       <HowItWork />
+      <Mission />
     </div>
   );
 }
